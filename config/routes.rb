ae2c0cf9f7a4b9cde63499805b735ddf44df4435
ninesteps <bowenjuice@gmail.com>
2016-07-13 03:07:34 +0000

    Add fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+   get 'freelancers/index'
   # You can have the root of your site routed with "root"
    root 'freelancers#index'
 
 
-      get 'freelancers/index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
