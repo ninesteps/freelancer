69bd66d2aa5eb0ff5bdfc18346485f80e556680d
ninesteps <bowenjuice@gmail.com>
2016-07-15 03:47:10 +0000

    updated sendmail failure message until I figure out heroku mail service

--- app/assets/javascripts/freelancer/contact_me.js
+++ app/assets/javascripts/freelancer/contact_me.js
@@ -20,8 +20,10 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+           
+           //Need to replace this with equivalent ruby and figure out heroku mailserver.
             $.ajax({
-                url: "././mail/contact_me.php",
+            url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -49,10 +51,9 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", this feature is currently unavailable, feel free to contact me at: emundwilson@gmail.com");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+
                 },
             })
         },
